fd29fe104c5bd39cddc07ede1a3e1cabc4af63b3
erocknine <echeungsk@gmail.com>
2020-06-26 00:32:45 -0400

    cleaned up unused components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import Navbar from './navbar.js'
 import Main from './main.js';
 import Welcome from './components/welcome.js'
-import HooliBox from './components/hoolibox.js'
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Redirect,
+//   Route
+// } from "react-router-dom";
 
-import React, { useState, useEffect, Fragment } from 'react';
+import React from 'react';
 
 function App() {
 
     return (
       <div>
-        <Router>
-          <Welcome />
-          <Navbar />
-          <Main />
-          {/* <Route exact path={`/hooli_box`} render={routeProps => <HooliBox />}/>
-          <Route exact path={`/philosophy`} render={routeProps => <Philosophy />}/> */}
-        </Router>
+        <Welcome />
+        <Navbar />
+        <Main />
       </div>
     );
 }
